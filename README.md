bd09ab97888b159caa995bc30db173ed66cf4468
羅云謙 <chien.lo@syntecclub.com.tw>
2024-02-02 17:28:31 +0800

    feat: insert data-cy tag with unique string

--- README.md
+++ README.md
@@ -1,3 +1,38 @@
 # vscode-auto-cypress-tag
 
-A simple VScode extension to add data-cy tag to your .vue file.
+## Overview
+
+VSCode Auto Cypress Tag is a simple extension for Visual Studio Code that helps you add `data-cy` attributes to HTML tags in your Vue files. It automates the process of generating unique identifiers for Cypress testing.
+
+## Features
+
+- Adds `data-cy` attributes to HTML tags in Vue files.
+- Generates unique identifiers using UUIDs.
+- Avoids specified tags (e.g., TEMPLATE, SCRIPT) based on user preferences.
+
+## Installation
+
+1. Open Visual Studio Code.
+2. Go to the Extensions view by clicking on the Extensions icon in the Activity Bar on the side of the window or use the keyboard shortcut `Ctrl + Shift + X`.
+3. Search for "VSCode Auto Cypress Tag".
+4. Click the "Install" button.
+
+## Usage
+
+1. Open a Vue file in Visual Studio Code.
+2. Click on the "Add Cypress Tag" button in the status bar.
+3. The extension will add `data-cy` attributes to relevant HTML tags in the file.
+
+## Configuration
+
+You can configure the extension by modifying the settings in your `settings.json` file.
+
+- `"autoCypressTag.tagsToAvoid"`: Specify tags to avoid adding `data-cy` attributes.
+
+## Contribution
+
+Feel free to contribute to this project by reporting issues or submitting pull requests on [GitHub](https://github.com/YUN-CHIEN-LO/vscode-auto-cypress-tag.git).
+
+## License
+
+This extension is licensed under the [MIT License](LICENSE.md).
--- src/extension.ts
+++ src/extension.ts
@@ -1,48 +1,98 @@
 import * as vscode from "vscode";
 
-/**
- * @param {vscode.ExtensionContext} context
- */
-function activate(context: vscode.ExtensionContext) {
-  try {
-    console.log("Extension activated!");
-
-    const statusBarItem = vscode.window.createStatusBarItem(
-      vscode.StatusBarAlignment.Left,
-      10
-    );
-    statusBarItem.text = "$(tag) Cypress Tag";
-    statusBarItem.tooltip = "Add Cypress Tag";
-    statusBarItem.command = "vscode-auto-cypress-tag.addCypressTag";
-    statusBarItem.show();
-
-    context.subscriptions.push(statusBarItem);
-    console.log("Status bar item added to subscriptions");
-
-    // Register the command
-    const disposable = vscode.commands.registerCommand(
-      "vscode-auto-cypress-tag.addCypressTag",
-      () => {
-        const editor = vscode.window.activeTextEditor;
-
-        if (editor) {
-          const position = editor.selection.active;
-          editor.edit((editBuilder) => {
-            editBuilder.insert(position, "add cypress tag");
-          });
-        }
-      }
-    );
-
-    context.subscriptions.push(disposable);
-  } catch (error) {
-    console.error("Error during activation:", error);
-    vscode.window.showErrorMessage(
-      "Extension activation failed. Check output for details."
-    );
-  }
+function isVueFile(document: vscode.TextDocument): boolean {
+  return document.languageId === "vue";
 }
 
-function deactivate() {}
+function generateUUID(): string {
+  return "xxxxxxxx".replace(/[xy]/g, function (c) {
+    const r = (Math.random() * 16) | 0;
+    const v = c === "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
 
-export { activate, deactivate };
+function addDataCyAttribute(editor: vscode.TextEditor): void {
+  const document = editor.document;
+  const edit = new vscode.WorkspaceEdit();
+
+  for (let i = 0; i < document.lineCount; i++) {
+    const line = document.lineAt(i);
+    const regex = /<([a-zA-Z0-9-]+)(?=\s|>)([^>]*)>/g;
+
+    let match;
+    while ((match = regex.exec(line.text)) !== null) {
+      const tagName = match[1];
+      const attributes = match[2];
+
+      // Check if the tag is in the list to be avoided
+      const tagNamesToAvoid = [
+        "TEMPLATE",
+        "SCRIPT",
+        "STYLE",
+        "TRANSITION",
+        "TRANSITION-GROUP",
+        "KEEPALIVE",
+        "TELEPORT",
+        "SUSPENSE",
+      ];
+      if (tagNamesToAvoid.includes(tagName.toUpperCase())) {
+        // Skip adding 'data-cy' attribute for this tag
+        console.log(`Skipping tag: ${tagName}`);
+        continue;
+      }
+
+      // Check if the tag already has a 'data-cy' attribute
+      if (!/\bdata-cy\b/.test(attributes)) {
+        // Add 'data-cy' attribute inside the tag
+        const startPosition = new vscode.Position(
+          i,
+          match.index + match[0].length - 1
+        );
+        const endPosition = new vscode.Position(
+          i,
+          match.index + match[0].length - 1
+        );
+        const range = new vscode.Range(startPosition, endPosition);
+        const text = ` data-cy="${tagName}-${generateUUID()}"`;
+
+        edit.insert(document.uri, range.end, text);
+      }
+    }
+  }
+
+  vscode.workspace.applyEdit(edit);
+}
+
+export function activate(context: vscode.ExtensionContext) {
+  const addTagButton = vscode.window.createStatusBarItem(
+    vscode.StatusBarAlignment.Left
+  );
+  addTagButton.text = "$(tag) Add data-cy Attribute";
+  addTagButton.command = "vscode-auto-cypress-tag.addCypressTag";
+  context.subscriptions.push(addTagButton);
+
+  const addDataCyAttributeCommand = vscode.commands.registerCommand(
+    "vscode-auto-cypress-tag.addCypressTag",
+    () => {
+      const editor = vscode.window.activeTextEditor;
+
+      if (editor) {
+        const document = editor.document;
+
+        if (isVueFile(document)) {
+          addDataCyAttribute(editor);
+        } else {
+          vscode.window.showErrorMessage(
+            "This command can only be applied to Vue files."
+          );
+        }
+      }
+    }
+  );
+
+  context.subscriptions.push(addDataCyAttributeCommand);
+  addTagButton.show();
+}
+
+export function deactivate() {}
